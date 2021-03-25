2230521f31b433db931257e89fefab0eac245997
Nived <nived@alignminds.net>
2021-03-25 16:22:14 +0530

    pushing new changes

--- public/js/invoice.js
+++ public/js/invoice.js
@@ -49,24 +49,7 @@ $(document).ready(function(){
 			$('#amountDue').val(totalAftertax);
 		}	
 	});	
-	$(document).on('click', '.deleteInvoice', function(){
-		var id = $(this).attr("id");
-		if(confirm("Are you sure you want to remove this?")){
-			$.ajax({
-				url:"action.php",
-				method:"POST",
-				dataType: "json",
-				data:{id:id, action:'delete_invoice'},				
-				success:function(response) {
-					if(response.status == 1) {
-						$('#'+id).closest("tr").remove();
-					}
-				}
-			});
-		} else {
-			return false;
-		}
-	});
+	
 });	
 function calculateTotal(){
 	var totalAmount = 0; 
